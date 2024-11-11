1689bbb684d85ad43f657aa210840f0753b3181d
gaow <1225039937@qq.com>
2024-11-11 13:58:53 +0800

    first commit

---
+++ play.js
@@ -0,0 +1,71 @@
+function initTools() {
+    console.log('爱壹帆TV助手 load success.')
+    const video_id = 'video_player'
+    const video_wrapper_class = 'video-player'
+    const ad_class = ['pggf', 'dabf', 'overlay-logo']
+    const video_logo_class = ''
+    const video_wrapper_top_class = 'aa-videoplayer-wrap'
+    const video_box_class = 'video-box'
+
+// 1.remove
+// ad
+    for (let i in ad_class) {
+        document.getElementsByClassName(ad_class[i]) && document.getElementsByClassName(ad_class[i])[0].remove()
+    }
+
+// 2.resize player
+    const video_player_wrapper = document.getElementsByClassName(video_wrapper_class)[0]
+    video_player_wrapper.style.width = '100%'
+
+// 3.add action
+    const video_player = document.getElementById('video_player')
+    const main_player = document.getElementById('main-player')
+    const action_wrap = document.getElementsByTagName('vg-controls')[1]
+
+    const pictureInPicture = document.createElement('div');
+    pictureInPicture.className = 'control-item'
+    pictureInPicture.innerText = '画中画'
+    pictureInPicture.style.marginRight = '10px'
+    pictureInPicture.style.color = '#ffffffb3'
+    pictureInPicture.style.cursor = 'pointer'
+    pictureInPicture.addEventListener('click', function () {
+        video_player.requestPictureInPicture().then(() => {
+            console.log('requestPictureInPicture success.')
+        })
+    })
+    action_wrap.lastChild.appendChild(pictureInPicture)
+
+    const fullWindow = document.createElement('div');
+    fullWindow.className = 'control-item'
+    fullWindow.innerText = '网页全屏'
+    fullWindow.style.marginRight = '10px'
+    fullWindow.style.color = '#ffffffb3'
+    fullWindow.style.cursor = 'pointer'
+    fullWindow.addEventListener('click', function () {
+        if (main_player.classList.contains('fullscreen')) {
+            main_player.classList.remove('fullscreen')
+            document.body.style.overflow = ''
+        } else {
+            main_player.classList.add('fullscreen')
+            document.body.style.overflow = 'hidden'
+        }
+    })
+    action_wrap.lastChild.appendChild(fullWindow)
+
+    const add_css = '*::-webkit-scrollbar { width: 0 !important; } #main-player.fullscreen { z-index: 9999; }'
+    let style = document.createElement('style')
+    document.head.appendChild(style)
+    style.type = 'text/css'
+    if (style.styleSheet) {
+        style.styleSheet.cssText = add_css
+    } else {
+        style.appendChild(document.createTextNode(add_css))
+    }
+}
+
+let sh = setInterval(() => {
+    if (document.getElementById('main-player')) {
+        initTools()
+        clearInterval(sh)
+    }
+}, 200)
